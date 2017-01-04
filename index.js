f2d6848009bf71100a5d262591b38c8d8cf9d8e8
newbienewbie <webbot@sina.com>
2017-01-04 15:35:25 +0800

    截取Excel第[start,end)行

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const {retrieveDataSource,construct,fetchAll}=require('./lib');
 const sqlFile=fs.createWriteStream(__dirname + '/sql.txt', {flags : 'w'});;
 const defaultFilePath=path.join(__dirname,"goods.xls")
 
-// 截取Excel第273行~1200行
-retrieveDataSource(defaultFilePath,272,1199)
+
+// 截取的范围是[start,end)，zero-based
+// 截取Excel第2~2421行(第一行是列名称)
+retrieveDataSource(defaultFilePath,1,2422)
     .then(list=> list.map(i=>{
         return {
             id:i.id,
